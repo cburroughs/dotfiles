8ed60161045bc5b570f05776c6e741e1f09252b0
Chris Burroughs <chris@chris-laptop.(none)>
2008-09-15 08:36:55 -0400

    Can't use X clipboard when not using X.

--- emacs/misc.el
+++ emacs/misc.el
@@ -4,8 +4,10 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 
 ;; copy-paste should work with other X clients
-(setq x-select-enable-clipboard t)
-(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+; duh; only do this when you have x
+(when window-system
+  (setq x-select-enable-clipboard t)
+  (setq interprogram-paste-function 'x-cut-buffer-or-selection-value))
 
 ;; change spelling
 (setq-default ispell-program-name "aspell")
