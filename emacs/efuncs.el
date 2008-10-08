2bf25b0199efc16dc424c7835e65cee10ae4943c
cburroughs <chris.burroughs@gmail.com>
2008-10-07 21:54:17 -0400

    f11 for maximize

--- emacs/efuncs.el
+++ emacs/efuncs.el
@@ -66,6 +66,15 @@
       (while (search-forward "\r" nil t) (replace-match "")))
 
 
+;; from emacs wiki
+;; obviously this would not work without X 
+(defun fullscreen ()
+  (interactive)
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                         '(2 "_NET_WM_STATE_FULLSCREEN" 0)))
+
+(global-set-key [f11] 'fullscreen)
+
 
 ; TODO: Investigate these
 ;http://dotfiles.org/~coder_/.emacs
