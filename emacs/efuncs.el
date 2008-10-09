bae1bf8732e5ba15ea6b19eb873834dae64d1940
cburroughs <chris.burroughs@gmail.com>
2008-10-08 20:44:13 -0400

    resize to 80 chars functions

--- emacs/efuncs.el
+++ emacs/efuncs.el
@@ -107,3 +107,27 @@
   "Hey I just want to run bash"
   (interactive)
   (ansi-term "/bin/bash"))
+
+;;http://dse.livejournal.com/67732.html
+(defun fix-frame-horizontal-size (width)
+  "Set the frame's size to 80 (or prefix arg WIDTH) columns wide."
+  (interactive "P")
+  (if window-system
+      (set-frame-width (selected-frame) (or width 80))
+    (error "Cannot resize frame horizontally: is a text terminal")))
+
+(defun fix-window-horizontal-size (width)
+  "Set the window's size to 80 (or prefix arg WIDTH) columns wide."
+  (interactive "P")
+  (enlarge-window (- (or width 80) (window-width)) 'horizontal))
+
+(defun fix-horizontal-size (width)
+  "Set the window's or frame's width to 80 (or prefix arg WIDTH)."
+  (interactive "P")
+  (condition-case nil
+      (fix-window-horizontal-size width)
+    (error 
+     (condition-case nil
+	 (fix-frame-horizontal-size width)
+       (error
+	(error "Cannot resize window or frame horizontally"))))))
