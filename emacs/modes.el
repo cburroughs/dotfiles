d7a96dbbe7bd6a0f7c9d5397a46ced1c86b00806
cburroughs <chris.burroughs@gmail.com>
2009-01-21 13:24:02 -0500

    js-commit hook

--- emacs/modes.el
+++ emacs/modes.el
@@ -35,7 +35,14 @@
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
-
+(require 'js-comint)
+(setq inferior-js-program-command "rhino")
+(add-hook 'js2-mode-hook '(lambda () 
+                            (local-set-key "\C-x\C-e" 'js-send-last-sexp)
+                            (local-set-key "\C-\M-x" 'js-send-last-sexp-and-go)
+                            (local-set-key "\C-cb" 'js-send-buffer)
+                            (local-set-key "\C-c\C-b" 'js-send-buffer-and-go)
+                            (local-set-key "\C-cl" 'js-load-file-and-go)))
 
 ;; Better buffer switching
 (require 'iswitchb)
