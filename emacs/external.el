16a2a5e199e6028557e0bcb5bf1904f34d20ab7e
cburroughs <chris.burroughs@gmail.com>
2009-01-21 13:04:43 -0500

    tidy!

---
+++ emacs/external.el
@@ -0,0 +1,8 @@
+;; talk to external programs
+
+;; keep things clean!
+
+(autoload 'tidy-buffer "tidy" "Run Tidy HTML parser on current buffer" t)
+(autoload 'tidy-parse-config-file "tidy" "Parse the `tidy-config-file'" t)
+(autoload 'tidy-save-settings "tidy" "Save settings to `tidy-config-file'" t)
+
