9fb93be8d5992ebde8e0c6a2604622e8a976e27f
cburroughs <chris.burroughs@gmail.com>
2009-01-26 11:58:51 -0500

    ubuntu lacks this package

--- emacs/modes.el
+++ emacs/modes.el
@@ -193,8 +193,11 @@
 
 ;; I always end up switching to the better mode anyway
 (add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
-(add-to-list 'auto-mode-alist '("\\.html$" . nxhtml-mode))
-(add-to-list 'auto-mode-alist '("\\.rhtml$" . nxhtml-mode))
+
+;; blah, ubuntu land lacks packages :-(
+(when (my-gentoo?)
+  (add-to-list 'auto-mode-alist '("\\.html$" . nxhtml-mode))
+  (add-to-list 'auto-mode-alist '("\\.rhtml$" . nxhtml-mode)))
 
 ;; colorize diffs.
 (eval-after-load 'diff-mode
