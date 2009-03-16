43d5b41a31278f89176ed3a6701bb898bd7e73c6
cburroughs <chris.burroughs@gmail.com>
2009-03-16 11:01:06 -0400

    Hey, that's pretty smart http://github.com/EnigmaCurry/emacs/commit/579b484107f24c39fdabd5505f8098f4605ec79e

--- emacs/pretty.el
+++ emacs/pretty.el
@@ -46,3 +46,18 @@
 (global-set-key (kbd "C-+")      '(lambda nil (interactive) (djcb-zoom 1)))
 (global-set-key (kbd "C--")      '(lambda nil (interactive) (djcb-zoom -1)))
 
+
+(defun smart-split ()
+  "Split the frame into 80-column sub-windows, and make sure no window has
+fewer than 80 columns."
+                                        ; From http://hjiang.net/archives/253
+  (interactive)
+  (defun smart-split-helper (w)
+    "Helper function to split a given window into two, the first of which has
+80 columns."
+    (if (> (window-width w) (* 2 81))
+        (let ((w2 (split-window w 82 t)))
+          (smart-split-helper w2))))
+  (smart-split-helper nil))
+
+
