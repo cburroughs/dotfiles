17d3ce975296517cfc7064600b5db698eb9728c3
cburroughs <chris.burroughs@gmail.com>
2009-07-02 09:01:30 -0400

    O RLY?

--- emacs/prog-modes.el
+++ emacs/prog-modes.el
@@ -202,3 +202,9 @@
 
 ;;; Scheme
 ;; see external.el for scheme program name 
+
+
+;; C#, YA RLY
+(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
+(setq auto-mode-alist
+      (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
