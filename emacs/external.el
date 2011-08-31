648c2b09cecc2bd062f46c58e4e882640acaf06c
cburroughs <chris.burroughs@gmail.com>
2011-08-31 10:09:07 -0400

    Trying to make google sites less painful than MS word.

--- emacs/external.el
+++ emacs/external.el
@@ -8,3 +8,11 @@
 
 ;; use mzscheme, obviously it must be installed
 (setq scheme-program-name "mzscheme")
+
+(defun p-markdown->html (beg end)
+  (interactive "r")
+  (shell-command-on-region beg end "pandoc -f markdown -t html"  nil 't))
+
+(defun p-html->markdown (beg end)
+  (interactive "r")
+  (shell-command-on-region beg end "pandoc -f html -t markdown"  nil 't))
