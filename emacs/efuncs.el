4bef48aba2f793f007f7a0bb9ed600da80fa51ba
cburroughs <chris.burroughs@gmail.com>
2015-11-24 14:37:55 -0500

    more handy functions

--- emacs/efuncs.el
+++ emacs/efuncs.el
@@ -219,3 +219,14 @@ scan-error if not."
   (interactive "r")
   (browse-url (concat "http://www.google.com/search?ie=utf-8&oe=utf-8&q=" (buffer-substring beg end))))
 
+
+;; http://emacsredux.com/blog/2013/03/27/copy-filename-to-the-clipboard/
+(defun copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
