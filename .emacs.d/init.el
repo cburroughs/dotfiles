583be8e59b5fc8857ac4b9415ba4881d50fe7c06
cburroughs <chris.burroughs@gmail.com>
2016-09-24 13:12:46 -0400

    keep home a little cleaner

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -85,7 +85,7 @@
 
 ;; If I ever use customize crap I don't want it pooping on this file
 ;; I assume this is automatically loaded
-(setq custom-file "~/.emac.ds/.emacs-custom.el")
+(setq custom-file "~/.config/emacs/.emacs-custom.el")
 (load custom-file 'noerror)
 
 
@@ -133,7 +133,7 @@
 ;; scattered all over the file system!
 ;; but not in temp
 (defvar autosave-dir
- (concat "~/.emacs_auto/autosaves/"))
+ (concat "~/.config/emacs/autosaves/"))
 
 (make-directory autosave-dir t)
 
@@ -150,7 +150,7 @@
 ;; Put backup files (ie foo~) in one place too. (The backup-directory-alist
 ;; list contains regexp=>directory mappings; filenames matching a regexp are
 ;; backed up in the corresponding directory. Emacs will mkdir it if necessary.)
-(defvar backup-dir (concat "~/.emacs_auto/backups/"))
+(defvar backup-dir (concat "~/.config/emacs/backups/"))
 (setq backup-directory-alist (list (cons "." backup-dir)))
 
 (setq semanticdb-default-save-directory "/tmp/")
