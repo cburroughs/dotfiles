bfe30e8145b795f057b066066ac6fc42b93a3681
cburroughs <chris.burroughs@gmail.com>
2016-09-23 21:36:06 -0400

    tidy support to pkg
    Althoug the pkg is still fairly old, have not clearly tested.

--- .emacs.d/lisp/external.el
+++ .emacs.d/lisp/external.el
@@ -2,6 +2,9 @@
 
 ;; keep things clean!
 
+(use-package tidy
+             :ensure t
+             :pin melpa)
 (autoload 'tidy-buffer "tidy" "Run Tidy HTML parser on current buffer" t)
 (autoload 'tidy-parse-config-file "tidy" "Parse the `tidy-config-file'" t)
 (autoload 'tidy-save-settings "tidy" "Save settings to `tidy-config-file'" t)
