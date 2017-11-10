b4a7f870a4ed1069dfcd4d62caff1ba40da55dc8
cburroughs <chris.burroughs@gmail.com>
2017-11-10 16:48:01 -0500

    unfill paragraph

--- .emacs.d/lisp/efuncs.el
+++ .emacs.d/lisp/efuncs.el
@@ -229,3 +229,15 @@ scan-error if not."
     (when filename
       (kill-new filename)
       (message "Copied buffer file name '%s' to the clipboard." filename))))
+
+
+;;; https://www.emacswiki.org/emacs/UnfillParagraph
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+(define-key global-map "\M-Q" 'unfill-paragraph)
