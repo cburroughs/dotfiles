129aa33ebed61b58f68eb3818a0de2244407326f
cburroughs <chris.burroughs@gmail.com>
2017-11-25 20:03:41 -0500

    langtool (grammar check)

--- .emacs.d/lisp/external.el
+++ .emacs.d/lisp/external.el
@@ -10,3 +10,15 @@
 (defun p-html->markdown (beg end)
   (interactive "r")
   (shell-command-on-region beg end "pandoc -f html -t markdown"  nil 't))
+
+
+;; langtool grammar check
+(use-package langtool
+             :ensure t
+             :pin melpa
+             :config
+             (setq langtool-bin "/usr/bin/languagetool")
+             (setq langtool-disabled-rules '("WHITESPACE_RULE"
+                                             "EN_UNPAIRED_BRACKETS"
+                                             "COMMA_PARENTHESIS_WHITESPACE"
+                                             "EN_QUOTES")))
