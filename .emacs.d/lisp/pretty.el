0fc1f97ed774e478d022e2b150304cd8247ee41a
cburroughs <chris.burroughs@gmail.com>
2017-11-28 21:17:52 -0500

    simplify initial size to be more predictable

--- .emacs.d/lisp/pretty.el
+++ .emacs.d/lisp/pretty.el
@@ -39,10 +39,8 @@
 (set-frame-font "DejaVu Sans Mono-11")
 
 ;; Start nice and tall, but should still be 80 char wide
-;; TODO: revert to 1 when I figure out why 100% was too tall, task bar?
 (if window-system
-  (setq initial-frame-alist '((top . 1) (height . 63))))
-
+  (setq initial-frame-alist '((top . 0) (width . 80) (height . 52))))
 
 ;; http://emacs-fu.blogspot.com/2008/12/zooming-inout.html
 (defun djcb-zoom (n)
