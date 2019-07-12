51a396bf78efd287058e14a69f894735853aa512
cburroughs <chris.burroughs@gmail.com>
2019-07-12 16:22:04 -0400

    picking up more misc functions

--- .emacs.d/lisp/efuncs.el
+++ .emacs.d/lisp/efuncs.el
@@ -255,3 +255,15 @@ scan-error if not."
   (interactive "r")
   (align-regexp start end
                 "\\(\\s-*\\)&" 1 1 t))
+
+
+;; https://emacsredux.com/blog/2013/03/27/copy-filename-to-the-clipboard/
+(defun csb/copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
