b8c4dafd5e84979c68d1ba93ba70bacb506fcf89
cburroughs <chris.burroughs@gmail.com>
2019-07-12 16:37:25 -0400

    make futzing with paragraph filling a little easier

--- .emacs.d/lisp/efuncs.el
+++ .emacs.d/lisp/efuncs.el
@@ -267,3 +267,15 @@ scan-error if not."
     (when filename
       (kill-new filename)
       (message "Copied buffer file name '%s' to the clipboard." filename))))
+
+
+;; https://www.emacswiki.org/emacs/UnfillParagraph
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+;; Handy key definition
+(define-key global-map "\M-Q" 'unfill-paragraph)
