1092a13c6e73a5b10f33803d13981d23e6ec45fa
cburroughs <chris.burroughs@gmail.com>
2019-08-01 15:08:10 -0400

    I think I have lsp working for python

--- .emacs.d/lisp/prog-modes.el
+++ .emacs.d/lisp/prog-modes.el
@@ -61,7 +61,7 @@
 
 
 (setq inferior-js-program-command "rhino")
-(add-hook 'js2-mode-hook '(lambda () 
+(add-hook 'js2-mode-hook '(lambda ()
                             (local-set-key "\C-x\C-e" 'js-send-last-sexp)
                             (local-set-key "\C-\M-x" 'js-send-last-sexp-and-go)
                             (local-set-key "\C-cb" 'js-send-buffer)
@@ -106,7 +106,7 @@
 (defun faust-change-mind ()
   (interactive)
   (setq-default indent-tabs-mode nil))
-  
+
 
 (defun java-4-space ()
   (interactive)
@@ -149,9 +149,64 @@
   (c-set-offset 'arglist-cont-nonempty '+))
 
 
-;; (use-package lsp-mode
-;;   :pin melpa-stable
-;;   :hook (rust-mode . lsp-deferred)
-;;   :commands (lsp lsp-deferred))
-  
-  
+;; Language Server Time
+
+
+;; TODO: in future versions, check out lsp-deferred
+(use-package lsp-mode
+  :ensure t
+  :pin melpa-stable
+  :hook ((python-mode . lsp)
+         (python-mode . (lambda () (flymake-mode 0))))
+  :commands lsp
+  :config
+  ;; Remove this crazy hack after https://github.com/emacs-lsp/lsp-ui/issues/234
+  ;; is stable
+  (with-eval-after-load 'lsp
+    (run-at-time "1 sec" 4 'flymake-mode 0))
+  (setq lsp-auto-guess-root 't)
+
+
+  (defun csb/lsp-ui-doc-toggle ()
+    (interactive)
+    (if (get 'csb/lsp-ui-doc-toggle 'state)
+        (progn
+          (put 'csb/lsp-ui-doc-toggle 'state nil)
+          (lsp-ui-doc-hide))
+      (progn
+        (put 'csb/lsp-ui-doc-toggle 'state 't)
+        (lsp-ui-doc-show))))
+
+  (use-package lsp-ui
+    :ensure t
+    :pin melpa-stable
+    :commands lsp-ui-mode
+    :bind (("C-c q" . csb/lsp-ui-doc-toggle))
+    :config
+    ;; not a fan of so much noise
+    (setq lsp-ui-flycheck-enable nil)
+    (setq lsp-prefer-flymake :none)
+    ;; the foo/bar format is less useful than the built in regex
+    ;; revisit after https://github.com/emacs-lsp/lsp-mode/issues/784 (post 6.0)
+    (setq lsp-ui-imenu-enable nil)
+    ;; Remove this crazy hack after https://github.com/emacs-lsp/lsp-ui/issues/234
+    ;; is stable
+    (with-eval-after-load 'lsp-ui
+      (run-at-time "1 sec" 4 'flymake-mode 0))
+
+    (setq lsp-ui-sideline-enable nil)
+    (setq lsp-ui-peek-enable nil)
+    ;; I like the inline docs, but not how it pops up all the freaking time, see
+    ;; toggle fn above
+    (setq lsp-ui-doc-enable nil)
+    (setq lsp-ui-doc-header 't)
+    (setq lsp-ui-doc-include-signature 't)
+    (setq lsp-ui-doc-position 'top)
+    (setq lsp-ui-doc-use-childframe 't))
+
+  (use-package company-lsp
+    :ensure t
+    :pin melpa-stable
+    :commands company-lsp)
+    :config
+    (push 'company-lsp company-backends))
