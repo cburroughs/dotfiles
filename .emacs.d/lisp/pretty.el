f25ecad2b5b9a91b348788db8dabf3fc789e31bf
cburroughs <chris.burroughs@gmail.com>
2019-09-11 10:14:40 -0400

    fully comment out tabbar for now

--- .emacs.d/lisp/pretty.el
+++ .emacs.d/lisp/pretty.el
@@ -299,37 +299,37 @@ fewer than 80 columns."
 
 ;; TODO: Choose
 ;; tabbar is pretty awesome
-(require 'tabbar)
-;; (tabbar-mode t)
-;; (global-set-key [(C-S-iso-lefttab)] 'tabbar-backward) ; why funy binding?
-;; (global-set-key [(control tab)]       'tabbar-forward)
-(set-face-attribute
- 'tabbar-default-face nil
- :background "gray60")
-(set-face-attribute
- 'tabbar-unselected-face nil
- :background "gray85"
- :foreground "gray30"
- :box nil)
-(set-face-attribute
- 'tabbar-selected-face nil
- :background "#f2f2f6"
- :foreground "black"
- :box nil)
-(set-face-attribute
- 'tabbar-button-face nil
- :box '(:line-width 1 :color "gray72" :style released-button))
-(set-face-attribute
- 'tabbar-separator-face nil
- :height 0.7)
+;; (require 'tabbar)
+;; ;; (tabbar-mode t)
+;; ;; (global-set-key [(C-S-iso-lefttab)] 'tabbar-backward) ; why funy binding?
+;; ;; (global-set-key [(control tab)]       'tabbar-forward)
+;; (set-face-attribute
+;;  'tabbar-default-face nil
+;;  :background "gray60")
+;; (set-face-attribute
+;;  'tabbar-unselected-face nil
+;;  :background "gray85"
+;;  :foreground "gray30"
+;;  :box nil)
+;; (set-face-attribute
+;;  'tabbar-selected-face nil
+;;  :background "#f2f2f6"
+;;  :foreground "black"
+;;  :box nil)
+;; (set-face-attribute
+;;  'tabbar-button-face nil
+;;  :box '(:line-width 1 :color "gray72" :style released-button))
+;; (set-face-attribute
+;;  'tabbar-separator-face nil
+;;  :height 0.7)
 
-;; Don't cycle *scratch* and friends
-(setq tabbar-buffer-list-function
-      (lambda ()
-        (remove-if
-         (lambda(buffer)
-           (find (aref (buffer-name buffer) 0) " *"))
-         (buffer-list))))
+;; ;; Don't cycle *scratch* and friends
+;; (setq tabbar-buffer-list-function
+;;       (lambda ()
+;;         (remove-if
+;;          (lambda(buffer)
+;;            (find (aref (buffer-name buffer) 0) " *"))
+;;          (buffer-list))))
 
 
 (use-package dashboard
