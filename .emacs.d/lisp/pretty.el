3a64b3fef2b297e97982991eb7a3e704a28ee918
cburroughs <chris.burroughs@gmail.com>
2022-01-14 11:29:49 -0500

    save my modeline on emacs-28

--- .emacs.d/lisp/pretty.el
+++ .emacs.d/lisp/pretty.el
@@ -389,6 +389,21 @@ lines are selected, or the NxM dimensions of a block selection."
                    mode-line-misc-info)
                  (csb/mlscroll-mode-right-reserved))))
 
+;; Keep the modline unicode symbols single color
+;; Otherwise wit emacs-28 and harfbuzz we get colorfull madness
+;; https://archive.casouri.cat/note/2021/fontset/index.html
+(create-fontset-from-fontset-spec
+ (font-xlfd-name
+  (font-spec :name "DejaVu Sans Mono"
+             :size 11
+             :registry "fontset-csb modeline")))
+
+(set-fontset-font
+ "fontset-csb modeline"
+ 'unicode (font-spec :name "Symbola"))
+
+(set-face-attribute 'mode-line nil :fontset "fontset-csb modeline")
+
 
 ;; Creates a "draw" of sorts for minor modes
 ;; https://manuel-uberti.github.io/emacs/2018/03/10/moody-and-minions/
